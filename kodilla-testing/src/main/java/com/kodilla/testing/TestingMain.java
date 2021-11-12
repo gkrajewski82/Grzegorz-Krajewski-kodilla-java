99f0d97461a28a2a35ddc50d198a9fb845e91e15
Grzegorz Krajewski <g.krajewski82@gmail.com>
2021-11-12 09:36:57 +0100

    6.2 First unit test - correction

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -6,5 +6,20 @@ import com.kodilla.testing.calculator.Calculator;
 public class TestingMain {
     public static void main(String[] args) {
 
+        Calculator calculator = new Calculator();
+        int resultAdd = calculator.add(34, 76);
+        int resultSubstractBFromA = calculator.substractBFromA(34, 54);
+
+        if (resultAdd == 110) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("ERROR!");
+        }
+
+        if (resultSubstractBFromA == -20) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("ERROR!");
+        }
     }
 }
--- kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -1,27 +1,12 @@
 package com.kodilla.testing.calculator;
 
 public class Calculator {
-    int a;
-    int b;
 
-    public Calculator(int a, int b) {
-        this.a = a;
-        this.b = b;
-    }
-
-    public int getA() {
-        return a;
-    }
-
-    public int getB() {
-        return b;
-    }
-
-    public int addAToB() {
+    public int add(int a, int b) {
         return a + b;
     }
 
-    public int substractAFromB() {
+    public int substractBFromA(int a, int b) {
         return a - b;
     }
 }
