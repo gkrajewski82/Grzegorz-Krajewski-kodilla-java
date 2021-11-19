dd6cc34c729eed3681a5019ceb91545fb1610786
Grzegorz Krajewski <g.krajewski82@gmail.com>
2021-11-19 20:43:10 +0100

    7.2 immutable ForumUser

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.lambda.Processor;
 import com.kodilla.stream.reference.FunctionalCalculator;
@@ -28,5 +29,8 @@ public class StreamMain {
         poemBeautifier.beautify("Beautiful text", (text) -> "8 " + text + " " + text.substring(6).length());
         poemBeautifier.beautify("Beautiful text", (text) -> "CBA - " + text + " - ABC");
         poemBeautifier.beautify("Beautiful text", (text) -> text.replace(' ', '_'));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
