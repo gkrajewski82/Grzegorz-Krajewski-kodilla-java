621af46ee0bb049c541e47d951ac8ab16bf2c12a
Grzegorz Krajewski <g.krajewski82@gmail.com>
2021-11-22 22:58:46 +0100

    7.5 - array

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,30 @@
+package com.kodilla.stream.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ArrayOperationsTestSuite {
+
+    @Test
+    void testGetAverage() {
+        //Given
+        int[] numbers = new int[10];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        numbers[3] = 4;
+        numbers[4] = 5;
+        numbers[5] = 6;
+        numbers[6] = 7;
+        numbers[7] = 8;
+        numbers[8] = 9;
+        numbers[9] = 10;
+
+        //When
+        double average = ArrayOperations.getAverage(numbers);
+
+        //Then
+        assertEquals(5.5, average);
+    }
+}
