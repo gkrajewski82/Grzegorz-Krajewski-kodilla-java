dfb422060884f2bc4e042a95a6cb9f54744af974
Grzegorz Krajewski <g.krajewski82@gmail.com>
2022-01-12 19:13:46 +0100

    13.2 - serwis zamowień produktów - update

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/BookOrderRepository.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/BookOrderRepository.java
@@ -2,6 +2,7 @@ package com.kodilla.good.patterns.challenges;
 
 public class BookOrderRepository implements OrderRepository {
 
+    @Override
     public void createOrder(User user, Product book) {
         System.out.println("Order for: " + user + ", product: " + book);
     }
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/BookOrderService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/BookOrderService.java
@@ -2,11 +2,11 @@ package com.kodilla.good.patterns.challenges;
 
 public class BookOrderService implements OrderService {
 
+    @Override
     public boolean order(User user, Product book) {
         if (user != null && book != null) {
             return true;
-        } else {
-            return false;
         }
+        return false;
     }
 }
