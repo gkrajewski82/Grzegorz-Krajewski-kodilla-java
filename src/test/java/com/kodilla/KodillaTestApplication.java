a255612e2ead858a61841a0241ee9de76c81553c
Grzegorz Krajewski <g.krajewski82@gmail.com>
2022-04-09 13:35:16 +0200

    stan projektu przed modułem 24.1 - przygotowanie do patterns2

---
+++ src/test/java/com/kodilla/KodillaTestApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaTestApplication {
+
+    @Test
+    public void contextLoad() {
+
+    }
+}
