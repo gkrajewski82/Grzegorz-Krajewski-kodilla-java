8abd7f8dbfeab23302bf6986ef2803ae1fdcfb31
Grzegorz Krajewski <g.krajewski82@gmail.com>
2022-04-09 16:55:36 +0200

    stan projektu przed zadaniem 24.1

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/Authentictor.java
@@ -0,0 +1,14 @@
+package com.kodilla.patterns2.facade;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+public class Authentictor {
+
+    public boolean isAuthenticated(Long userId) {
+        Random random = new Random();
+        return random.nextBoolean();
+    }
+}
