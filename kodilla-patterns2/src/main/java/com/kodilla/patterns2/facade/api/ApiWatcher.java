a19c402240195763ac6eb8ab77dfe8fe04a6e62d
Grzegorz Krajewski <g.krajewski82@gmail.com>
2022-04-23 18:58:24 +0200

    24.3 - AOP

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/ApiWatcher.java
@@ -0,0 +1,20 @@
+package com.kodilla.patterns2.facade.api;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class ApiWatcher {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ApiWatcher.class);
+
+    @Before("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))" +
+            "&& args(orderDto, userId) && target(object)")
+    public void logEvent(OrderDto orderDto, Long userId, Object object) {
+        LOGGER.info("Class: " + object.getClass().getName() + ", args: " + "orderDto = " + orderDto + ", userId = " + userId);
+    }
+}
